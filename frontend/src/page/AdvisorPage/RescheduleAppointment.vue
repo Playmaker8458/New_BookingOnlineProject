3c4648ca7b3dbc10086873804ad27bff8af70344
Playmaker8458 <pongpolbank8458@gmail.com>
2026-02-21 19:36:26 +0700

    Update New ALL

--- frontend/src/page/AdvisorPage/RescheduleAppointment.vue
+++ frontend/src/page/AdvisorPage/RescheduleAppointment.vue
@@ -1,13 +1,13 @@
 <script setup>
     import NavbarAdvisor from '../../components/NavbarAdvisor.vue'
-    import FormRescheduleAppointment from '../../components/AdvisorContent/Form_RescheduleAppointment.vue' 
+    import FormRescheduleAppointmentAdvisor from '../../components/AdvisorContent/form_RescheduleAppointmentAdvisor.vue' 
     import Footer from '../../components/Footer.vue'
 </script>
 
 <template>
     <NavbarAdvisor />
     <main class="min-h-screen bg-[#EBE8E8]">
-        <FormRescheduleAppointment/>
+        <FormRescheduleAppointmentAdvisor/>
     </main>
     <Footer />
 </template>
--- frontend/src/page/StudentPage/RescheduleAppointment.vue
+++ frontend/src/page/StudentPage/RescheduleAppointment.vue
@@ -1,13 +1,13 @@
 <script setup>
     import NavbarStudent from '../../components/NavbarStudent.vue'
-    import FormRescheduleAppointment from '../../components/StudentContent/Form_RescheduleAppointment.vue' 
+    import FormRescheduleAppointmentStudent from '../../components/StudentContent/form_RescheduleAppointmentStudent.vue' 
     import Footer from '../../components/Footer.vue'
 </script>
 
 <template>
     <NavbarStudent />
     <main class="min-h-screen bg-[#EBE8E8]">
-        <FormRescheduleAppointment/>
+        <FormRescheduleAppointmentStudent/>
     </main>
     <Footer />
 </template>
